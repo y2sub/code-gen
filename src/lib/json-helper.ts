a20b5c20756f40900b1c80eeb9df6d3922ca955e
najib.elhusseini@hoist.tech <najib.elhusseini@hoist.tech>
2024-05-23 11:09:24 +0300

    Created json-helper.ts that contains custom checks

---
+++ src/lib/json-helper.ts
@@ -0,0 +1,44 @@
+
+export const maxInt32 = 0x7fffffff;
+
+export function revive(key: string, value: any, context: any) {
+    const source = context;
+    const dataType = typeof value
+    switch (dataType) {
+        case "number":
+            return `${context.source}`;
+        default:
+            break;
+    }
+
+    return value;
+}
+
+export function checkDataType(value: string | number | bigint | any): dataTypes {
+    const num = Number(value);
+    if (!isNaN(num)) {
+        if (`${value}`.includes('.')) {
+            return 'double';
+        } else {
+            return num >= maxInt32 ? 'long' : 'int';
+        }
+    }
+    let date = new Date(value);
+    if (!isNaN(date.getTime())) {
+        return 'DateTime';
+    }
+
+    return 'string'
+    // let dt = new Date(`${value}`);
+
+}
+
+export type dataTypes = 'string' | 'DateTime' | 'bool' | 'int' | 'long' | 'double' | 'object' | 'list';
+export type ObjectMember = {
+    name: string;
+    dataType: dataTypes;
+    members?: ObjectMember[];
+    nullable?: boolean;
+    accessModifier: 'public' | 'private';
+    indent: number;
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,31 @@
 <script lang="ts">
-	type dataTypes = 'string' | 'DateTime' | 'bool' | 'int' | 'long' | 'double' | 'object' | 'list';
-	type ObjectMember = {
-		name: string;
-		dataType: dataTypes;
-		members?: ObjectMember[];
-		nullable?: boolean;
-		accessModifier: 'public' | 'private';
-		indent: number;
-	};
-	const maxInt32 = 0x7fffffff;
-
 	import Menu from '$lib/Menu.svelte';
-	let isDialogValid = true;
+	import { checkDataType, maxInt32, revive, type ObjectMember } from '$lib/json-helper';
+	let isJsonValid = true;
+	let jsonText: string = '';
+	let validationErrorText: string | undefined = undefined;
 	let diag: HTMLDialogElement;
 
+	function showDialog() {
+		isJsonValid = true;
+		validationErrorText = '';
+		diag.showModal();
+	}
+
 	function handleDialogSubmitted(event: Event) {
+		console.clear();
 		event.stopImmediatePropagation();
 		event.stopPropagation();
 		event.preventDefault();
 		const form = event.target as HTMLFormElement;
-		isDialogValid = true;
-		let isValid = form.checkValidity();
+		isJsonValid = true;
+		jsonText = jsonText.trim();
+
+		let isValid = jsonText !== '';
+
 		if (!isValid) {
+			isJsonValid = false;
+			validationErrorText = 'Please enter a valid, non empty JSON object';
 			return;
 		}
 		let data = new FormData(form);
@@ -30,17 +34,17 @@
 
 		let jsonObject: any | undefined = undefined;
 		try {
-			jsonObject = JSON.parse(jsonString, (_key: any, value: any, data: any) =>
-				typeof value === 'number' ? data.source : value
-			);
-			isValid = typeof jsonObject === 'object';
+			jsonObject = JSON.parse(jsonString, revive);
+			if (typeof jsonObject !== 'object') {
+				throw 'Invalid JSON object';
+			}
 		} catch (e) {
 			isValid = false;
-		}
-		if (!isValid) {
-			isDialogValid = false;
+			isJsonValid = false;
+			validationErrorText = 'Could not parse text into a valid JSON object';
 			return;
 		}
+
 		let baseObject: ObjectMember = {
 			name: 'BaseObject',
 			nullable: false,
@@ -51,7 +55,6 @@
 		};
 		let members = getObjectProperties(jsonObject, 1);
 		baseObject.members = members;
-		console.clear();
 
 		console.log(baseObject);
 	}
@@ -63,8 +66,8 @@
 		let objectMembers: ObjectMember[] = [];
 		let props = Object.getOwnPropertyNames(obj);
 		for (const propName of props) {
-			const fieldValue = obj[propName];
-			let fieldType = typeof fieldValue;
+			const value = obj[propName];
+			let dataType = typeof value;
 			let objectMember: ObjectMember = {
 				name: propName,
 				dataType: 'string',
@@ -73,32 +76,23 @@
 				members: undefined,
 				indent
 			};
-			if (Array.isArray(fieldValue)) {
+			if (Array.isArray(value)) {
 				objectMember.dataType = 'list';
 				objectMembers.push(objectMember);
 				continue;
 			}
-			switch (fieldType) {
+			switch (dataType) {
 				case 'bigint':
 				case 'number':
-					break;
 				case 'string':
-					const num = Number(fieldValue);
-					if (!isNaN(num)) {
-						if (`${fieldValue}`.includes('.')) {
-							objectMember.dataType = 'double';
-						} else {
-							objectMember.dataType = num >= maxInt32 ? 'long' : 'int';
-						}
-					}
-
+					objectMember.dataType = checkDataType(value);					
 					break;
 				case 'boolean':
 					objectMember.dataType = 'bool';
 					break;
 				case 'object':
 					objectMember.dataType = 'object';
-					objectMember.members = getObjectProperties(fieldValue, indent + 1);
+					objectMember.members = getObjectProperties(value, indent + 1);
 				case 'symbol':
 				case 'undefined':
 				case 'function':
@@ -127,12 +121,7 @@
 					<i class="bi bi-upload"></i>
 					<span>From File </span>
 				</button>
-				<button
-					type="button"
-					on:click={() => {
-						diag.showModal();
-					}}
-				>
+				<button type="button" on:click={showDialog}>
 					<i class="bi bi-braces" />
 					<span> Paste Json </span>
 				</button>
@@ -146,27 +135,29 @@
 	class="border border-neutral-500 p-2 w-full md:w-3/4 lg:w-2/3 xl:w-1/2 rounded shadow-lg
      backdrop:bg-white/20 bg-neutral-800 text-neutral-100"
 >
-	<form method="dialog" class="" on:submit={handleDialogSubmitted}>
+	<form method="dialog" class="" on:submit={handleDialogSubmitted} novalidate>
 		<h3 class="py-2 pb-4 border-b border-b-neutral-500 text-2xl space-x-1">
 			<i class="bi bi-braces" />
 			<span> Paste Json </span>
 		</h3>
 		<div class="grid gap-2 my-4">
 			<textarea
+				aria-invalid={!isJsonValid}
 				name="json"
-				required={true}
-				rows="5"
+				rows="7"
 				class="focus:outline-none border border-neutral-500 bg-black px-1 py-1 rounded
-                ring-sky-600/20 invalid:ring-pink-600/20
+                ring-sky-600/20 aria-[invalid='true']:ring-pink-600/20 aria-[invalid='true']:border-pink-600
                 focus:ring peer
                 "
+				bind:value={jsonText}
 			/>
 
-			<span
-				class="{isDialogValid ? 'invisible' : 'visible'} peer-invalid:visible text-sm text-pink-500"
-			>
-				Invalid JSON
-			</span>
+			<div class="text-center invisible peer-aria-[invalid='true']:visible text-sm">
+				<span class=" text-amber-500 space-x-2">
+					<i class="bi bi-exclamation-triangle-fill"></i>
+					{validationErrorText}
+				</span>
+			</div>
 		</div>
 		<div class="my-4 grid grid-cols-2 gap-x-2">
 			<button
